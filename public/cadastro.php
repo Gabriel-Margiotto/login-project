6312d56e63ea7db059150ce07ed4406cb719c9af
Gabriel Margiotto <gabimargi@gmail.com>
2026-02-06 16:16:14 -0300

    Adicionado estilização com Bootstrap.

--- public/cadastro.php
+++ public/cadastro.php
@@ -5,28 +5,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+    <?php include("../assets/css/bootstrap.php"); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"
+        defer></script>
 </head>
 
-<body>
-    <form action="cadastro.php" method="POST">
-        <label>
-            Nome:
-            <input type="text" name="name-cadastro" minlength="4" maxlength="50">
-        </label>
-        <label>
-            Email:
-            <input type="email" name="email-cadastro" minlength="4" maxlength="50">
-        </label>
-        <label>
-            Senha:
-            <input type="password" minlength="8" maxlength="16" name="password-cadastro">
-        </label>
-        <label>
-            Confirmar senha:
-            <input type="password" name="password-confirm" minlength="8" maxlength="16">
-        </label>
-        <button type="submit" name="bt-cadastrar">Cadastrar</button>
+<style>
+    .w {
+        width: 400px;
+    }
+</style>
+
+<body class="bg-light">
+    <form action="cadastro.php" method="POST" class="container-sm">
+        <div class="row m-5 flex-column">
+            <h1 class="text-center fw-bold text-uppercase mb-md-4">Cadastro</h1>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">Nome Completo</span>
+                <input type="text" class="form-control" name="name-cadastro" minlength="4" maxlength="50">
+            </div>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">E-mail</span>
+                <input type="text" class="form-control" name="email-cadastro" minlength="4" maxlength="50"
+                    placeholder="nome@exemplo.com">
+            </div>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">Senha</span>
+                <input type="password" class="form-control" name="password-cadastro" minlength="8" maxlength="16">
+            </div>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">Confirmar senha</span>
+                <input type="password" class="form-control" name="password-confirm" minlength="8" maxlength="16">
+            </div>
+            <button type="submit" name="bt-cadastrar"
+                class="w mt-2 rounded btn btn-outline-success align-self-center">Cadastrar</button>
+            <p class="text-center">Já possui uma conta? <a
+                    class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                    href="login.php">Login</a></p>
+
+            <div class="alert alert-danger w text-center align-self-center p-1 visually-hidden" role="alert">
+                Dados incorretos, revise!
+            </div>
+            <div class="alert alert-warning w text-center align-self-center p-1 visually-hidden" role="alert">
+                Esse e-mail já existe! <br> Clique para <a href="./login.php" class="alert-link">Entrar</a>.
+            </div>
+        </div>
     </form>
+
+
 </body>
 
 </html>
@@ -80,7 +107,7 @@ if (empty($_SESSION["email"])) {
             }
 
         } else {
-            echo "<br> Preencha os dados corretamente!";
+            // erro msg
 
         }
     } // Final IF botão de cadastro.
--- public/login.php
+++ public/login.php
@@ -5,21 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <?php include("../assets/css/bootstrap.php"); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"
+        defer></script>
 </head>
 
-<body>
-    <form action="login.php" method="POST">
-        <label>
-            Email:
-            <input type="email" name="email">
-        </label>
-        <label>
-            Senha:
-            <input type="password" name="password">
-        </label>
-        <button type="submit" name="bt-login">Logar</button>
-        <br>
-        <a href="cadastro.php">Cadastrar</a>
+<style>
+    .w {
+        width: 400px;
+    }
+</style>
+
+<body class="bg-light">
+    <form action="login.php" method="POST" class="container">
+        <div class="row flex-column m-5">
+            <h1 class="text-center fw-bold text-uppercase mb-md-4">Entrar</h1>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">E-mail</span>
+                <input type="email" class="form-control" name="email" minlength="10" maxlength="50"
+                    placeholder="nome@exemplo.com">
+            </div>
+            <div class="input-group mb-3 w p-0 align-self-center">
+                <span class="input-group-text">Senha</span>
+                <input type="password" class="form-control" name="password" minlength="8" maxlength="16">
+            </div>
+            <button type="button" name="bt-login"
+                class="btn btn-outline-success w align-self-center  mt-2">Logar</button>
+            <p class="text-center">Não possui uma conta? <a
+                    class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                    href="cadastro.php">Cadastrar</a></p>
+
+            <div class="alert alert-danger w p-2 align-self-center text-center visually-hidden" role="alert">
+                Email ou senha inválidos.
+            </div>
+        </div>
     </form>
 </body>
 
